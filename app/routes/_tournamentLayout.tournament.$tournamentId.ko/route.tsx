82acf6f94f2cb4ed28147733bbb512effdbeb6e3
josemlrod <mrjosemlrod@gmail.com>
2024-03-07 22:45:49 -0500

    update ko styles

--- app/routes/_tournamentLayout.tournament.$tournamentId.ko/route.tsx
+++ app/routes/_tournamentLayout.tournament.$tournamentId.ko/route.tsx
@@ -33,7 +33,7 @@ export default function Knockout() {
 
   return (
     <section className="h-full w-full flex flex-col py-4">
-      <div className="w-full grow flex gap-2 md:gap-4">
+      <div className="w-full grow flex gap-2 md:gap-4 flex-wrap">
         {/* Quarter finals column */}
         <div className="grow flex flex-col justify-center items-center min-w-[185px] gap-4">
           <div className="flex justify-center items-center bg-primary-foreground py-4 w-full rounded-xl">
